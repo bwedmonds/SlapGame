dc8a8a808fab778679f41a192b4089f90d2ea1a5
Bryce Edmonds <bryce@bryceedmonds.com>
2019-05-12 14:07:25 -0600

    day2 midday push

--- assets/game.js
+++ assets/game.js
@@ -1,51 +1,113 @@
-let name = "Zach"
-let health = 0;
-let hits = 0;
+// let name = "Zach"
+// let health = 0;
+// let hits = 0;
 
-let player = {
-  name: Zach,
+var player = {
+  name: 'Zach',
   smileyness: 0,
   smileyIndex: 0,
   frownPower: 50,
-  moods: ["specialfrown", "regularRBF", "pseudosmiley"]
+  // moods: ["specialfrown", "regularRBF", "pseudosmiley"],
   hits: 0,
-  images: ['/assets/images/zach_frown.png', '/assets/images/zach_rbf.png', '/assets/images/zach_selfie.png']
+  images: ['/assets/images/zach_frown.png', '/assets/images/zach_rbf.png', '/assets/images/zach_selfie.png'],
   items: []
 }
 
-function highFive() {
-  player.smileyness++;
-  player.hits++;
-  update()
+function increaseSmileyIndex() {
+  if (player.smileyness == player.frownPower) {
+    player.smileyIndex++;
+  } else if (player.smileyness == player.frownPower * 2) {
+    player.smileyIndex++;
+  }
+  drawZach()
 }
 
-function rumPunch() {
-  player.smileyness += 2;
-  player.hits += 1;
-  update()
+function drawZach() {
+  document.getElementById("zach-image").setAttribute("src", player.images[player.smileyIndex])
 }
 
-function cake() {
-  player.smileyness += 2;
+var items = {
+  bear: {
+    name: 'Bear',
+    modifier: 5,
+    description: 'So huggy!'
+  },
+  feather: {
+    name: 'Feather',
+    modifier: 5,
+    description: 'Not the feather!'
+  },
+  puppies: {
+    name: 'Puppies',
+    modifier: 10,
+    description: 'I. Can\'t. Resist!'
+  }
+}
+
+// function highFive() {
+//   player.smileyness++;
+//   player.hits++;
+//   update()
+// }
+
+// function rumPunch() {
+//   player.smileyness += 2;
+//   player.hits += 1;
+//   update()
+// }
+
+// function cake() {
+//   player.smileyness += 2;
+//   player.hits += 1;
+//   update();
+// }
+
+// function tickle() {
+//   player.smileyness += 3;
+//   player.hits += 1;
+//   update();
+// }
+
+// function cuddle() {
+//   player.smileyness += 3;
+//   player.hits += 1;
+//   update();
+// }
+
+// function hug() {
+//   player.smileyness += 3;
+//   player.hits += 1;
+//   update();
+// }
+
+function smilies(hitNum) {
+  player.smileyness += hitNum + addmods();
   player.hits += 1;
+  winner();
   update();
 }
 
-function tickle() {
-  player.smileyness += 3;
-  player.hits += 1;
-  update();
+function giveItem(item) {
+  if (player.items.length == 0) {
+    player.items.push(items[item].modifier);
+  } else {
+    player.items.pop();
+    player.items.push(items[item].modifier);
+  }
 }
 
-function cuddle() {
-  player.smileyness += 3;
-  player.hits += 1;
-  update();
+function addmods() {
+  let currentMods = 0;
+  for (let i = 0; i < player.items.length; i++) {
+    currentMods += player.items[i];
+  }
+  return currentMods;
 }
 
-function hug() {
-  player.smileyness += 3;
-  player.hits += 1;
+function reset() {
+  player.smileyness = 0;
+  player.hits = 0;
+  player.items.pop();
   update();
 }
 
@@ -53,11 +115,17 @@ function update() {
   // document.getElementById("smileyness").innerText = player.smileyness;
   document.getElementById('hits').innerText = player.hits.toString();
   document.getElementById('name').innerText = player.name;
-  document.getElementById('smileyness').innerText = player.smileyness.toString();
-  document.getElementById('smileyness').setAttribute('style', `width: ${player.smileyness.toString()}%`)
+  document.getElementById('player-smileyness').innerText = player.smileyness.toString();
+  document.getElementById('player-smileyness').setAttribute('style', `width: ${player.smileyness.toString()}%`)
 }
 
-update()
-highFive()
-rumPunch()
-cake()
+function winner() {
+  if (player.smileyness >= 100) {
+    alert('What? Zach smiled?! Inconceivable! Reset to defeat RBF again!');
+  }
+}
+
+// update()
+// highFive()
+// rumPunch()
+// cake()
