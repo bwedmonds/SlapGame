390e0a5390120ed791cbf3cf10e7818788d563e6
Bryce Edmonds <bryce@bryceedmonds.com>
2019-05-11 18:22:14 -0600

    EOD Day1 push

--- assets/game.js
+++ assets/game.js
@@ -19,17 +19,36 @@ function rumPunch() {
 function cake() {
   health += 2;
   hits += 1;
-  // alert(health);
+  update();
+}
+
+function tickle() {
+  health += 3;
+  hits += 1;
+  update();
+}
+
+function cuddle() {
+  health += 3;
+  hits += 1;
+  update();
+}
+
+function hug() {
+  health += 3;
+  hits += 1;
   update();
 }
 
 function update() {
-  document.getElementById("health").innerText = health
-  document.getElementById("hits").innerText = hits
-  document.getElementById("name").innerText = name
-
+  // document.getElementById("health").innerText = health;
+  document.getElementById('hits').innerText = hits.toString();
+  document.getElementById('name').innerText = name;
+  document.getElementById('health').innerText = health.toString();
+  document.getElementById('health').setAttribute('style', `width: ${health.toString()}%`)
 }
 
+update()
 // highFive()
 // rumPunch()
 // cake()
